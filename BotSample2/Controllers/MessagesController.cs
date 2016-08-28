0acc610b96f82c66af4b74f9f3de3a2ac47a101c
Satoshi Matsushita <simeon_the_city_who_fought@outlook.com>
2016-08-28 17:40:16 +0900

    Add Simple sandwich

--- BotSample2/Controllers/MessagesController.cs
+++ BotSample2/Controllers/MessagesController.cs
@@ -7,6 +7,9 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using BotSample2.Dialogs;
 
 namespace BotSample2
 {
@@ -19,51 +22,22 @@ namespace BotSample2
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
+            switch (activity?.GetActivityType())
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                case ActivityTypes.Message:
+                    await Conversation.SendAsync(activity, MakeRootDialog);
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
-            }
-            else
-            {
-                HandleSystemMessage(activity);
+                    break;
+                default:
+                    break;
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
+        internal static IDialog<SandwichOrder> MakeRootDialog() {
+            return Chain.From(() => FormDialog.FromForm(SandwichOrder.BuildForm));
 
-            return null;
         }
     }
 }
---
+++ BotSample2/Dialogs/CustomerOrder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.FormFlow;
+
+
+
+namespace BotSample2.Dialogs
+{
+    [Serializable]
+    public class SandwichOrder
+    {
+        public SandwichOptions? Sandwich;
+        public LengthOptions? Length;
+        public BreadOptions? Bread;
+        public CheeseOptions? Cheese;
+        public List<ToppingOptions> Toppings;
+        public List<SauceOptions> Sauce;
+        public static IForm<SandwichOrder> BuildForm()
+        {
+            return new FormBuilder<SandwichOrder>()
+                    .Message("サンドイッチボットへようこそ♪")
+                    .Build();
+        }
+    }
+
+
+    #region enum
+
+    public enum SandwichOptions
+    {
+        BLT,
+        BlackForestHam,
+        BuffaloChicken,
+        ChickenAndBaconRanchMelt,
+        ColdCutCombo,
+        MeatballMarinara,
+        OvenRoastedChicken,
+        RoastBeef,
+        RotisserieStyleChicken,
+        SpicyItalian,
+        SteakAndCheese,
+        SweetOnionTeriyaki,
+        Tuna,
+        TurkeyBreast,
+        Veggie
+    }
+
+
+
+    public enum LengthOptions { SixInch, FootLong }
+    public enum BreadOptions { NineGrainWheat, NineGrainHoneyOat, Italian, ItalianHerbsAndCheese, Flatbread }
+    public enum CheeseOptions { American, MontereyCheddar, Pepperjack }
+    public enum ToppingOptions
+    {
+        Avocado,
+        BananaPeppers,
+        Cucumbers,
+        GreenBellPeppers,
+        Jalapenos,
+        Lettuce,
+        Olives,
+        Pickles,
+        RedOnion,
+        Spinach,
+        Tomatoes
+    }
+
+
+
+    public enum SauceOptions
+    {
+        ChipotleSouthwest,
+        HoneyMustard,
+        LightMayonnaise,
+        RegularMayonnaise,
+        Mustard,
+        Oil,
+        Pepper,
+        Ranch,
+        SweetOnion,
+        Vinegar
+    }
+
+    #endregion
+
+}
